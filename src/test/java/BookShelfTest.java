97df4f5d521f71bee2a8087fcd93729464947284
PetterBT <petter.bergstrom@iths.se>
2022-06-03 11:40:39 +0200

    Add constructor

--- src/test/java/BookShelfTest.java
+++ src/test/java/BookShelfTest.java
@@ -2,10 +2,19 @@
 
 import bookstore.BookShelf;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+
 import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class BookShelfSpec {
+class BookShelfTest {
+
+    /**
+     * @param testInfo Contains information about the current test.
+     */
+    private BookShelfTest(TestInfo testInfo) {
+        System.out.println("Working on test " + testInfo.getDisplayName());
+    }
 
     @Test
     public void shelfEmptyWhenNoBookAdded() throws Exception {
