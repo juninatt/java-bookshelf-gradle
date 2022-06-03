e30c4c921d40d9d0b9925a340270e5853873b2d2
PetterBT <petter.bergstrom@iths.se>
2022-06-03 11:53:00 +0200

    Remove constructor and add parameter to test method

--- src/test/java/BookShelfTest.java
+++ src/test/java/BookShelfTest.java
@@ -1,23 +1,23 @@
 
 
 import bookstore.BookShelf;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInfo;
 
 import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@DisplayName("<= BookShelf Tests =>")
 class BookShelfTest {
 
     /**
-     * @param testInfo Contains information about the current test.
+     * @param testInfo Contains information about the current test
      */
-    private BookShelfTest(TestInfo testInfo) {
-        System.out.println("Working on test " + testInfo.getDisplayName());
-    }
-
     @Test
-    public void shelfEmptyWhenNoBookAdded() throws Exception {
+    @DisplayName("<= Shelf should return empty when no books added =>")
+    void shelfEmptyWhenNoBookAdded(TestInfo testInfo) {
+        System.out.println("Working on test case " + testInfo.getDisplayName());
         BookShelf shelf = new BookShelf();
         List<String> books = shelf.books();
         assertTrue(books.isEmpty(), () -> "BookShelf should be empty.");
