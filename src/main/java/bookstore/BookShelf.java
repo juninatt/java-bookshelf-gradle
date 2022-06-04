4a96b17b55dcbb8bfd292d96ae6970a845f6c4ad
PetterBT <petter.bergstrom@iths.se>
2022-06-04 15:32:08 +0200

    Refactor add method to handle several String-objects as parameters

--- src/main/java/bookstore/BookShelf.java
+++ src/main/java/bookstore/BookShelf.java
@@ -1,14 +1,15 @@
 package bookstore;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class BookShelf {
 
     private final List<String> books = new ArrayList<>();
 
-    public void add(String book) {
-        books.add(book);
+    public void add(String... booksToAdd) {
+        books.addAll(Arrays.asList(booksToAdd));
     }
     public List<String> getBooks() {
         return books;
