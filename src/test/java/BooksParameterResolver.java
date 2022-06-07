af9730cc0c49e7ebe0bc2682856a132444684761
PetterBT <petter.bergstrom@iths.se>
2022-06-07 19:36:19 +0200

    Add Constructor
    - add constructor
    - move sample data creation to constructor

--- src/test/java/BooksParameterResolver.java
+++ src/test/java/BooksParameterResolver.java
@@ -13,6 +13,18 @@ import java.util.Objects;
 
 class BooksParameterResolver implements ParameterResolver {
 
+    private final Map<String, Book> books;
+
+    public BooksParameterResolver() {
+        Map<String, Book> books = new HashMap<>();
+        books.put("Spring in Action", new Book("Spring in Action, 6th Edition", "Craig Walls", LocalDate.of(2022, Month.JANUARY, 1)));
+        books.put("Effective Java", new Book("Effective Java", "Joshua Bloch", LocalDate.of(2008, Month.MAY, 8)));
+        books.put("Half a War", new Book("Half a War", "Joe Abercrombie", LocalDate.of(2016, Month.MARCH, 8)));
+        books.put("Spring Security in Action", new Book("Spring Security in Action", "Laurentiu Spilca", LocalDate.of(2020, Month.JANUARY, 1)));
+        books.put("Spring Boot in Action", new Book("Spring Boot in Action", "Craig Walls", LocalDate.of(2016, Month.JANUARY, 1)));
+        this.books = books;
+    }
+
     @Override
     public boolean supportsParameter(final ParameterContext parameterContext,
                                      final ExtensionContext extensionContext) throws ParameterResolutionException {
@@ -24,12 +36,6 @@ class BooksParameterResolver implements ParameterResolver {
     @Override
     public Object resolveParameter(final ParameterContext parameterContext,
                                    final ExtensionContext extensionContext) throws ParameterResolutionException {
-        Map<String, Book> books = new HashMap<>();
-        books.put("Spring in Action", new Book("Spring in Action, 6th Edition", "Craig Walls", LocalDate.of(2022, Month.JANUARY, 1)));
-        books.put("Effective Java", new Book("Effective Java", "Joshua Bloch", LocalDate.of(2008, Month.MAY, 8)));
-        books.put("Half a War", new Book("Half a War", "Joe Abercrombie", LocalDate.of(2016, Month.MARCH, 8)));
-        books.put("Spring Security in Action", new Book("Spring Security in Action", "Laurentiu Spilca", LocalDate.of(2020, Month.JANUARY, 1)));
-        books.put("Spring Boot in Action", new Book("Spring Boot in Action", "Craig Walls", LocalDate.of(2016, Month.JANUARY, 1)));
         return books;
     }
 }
